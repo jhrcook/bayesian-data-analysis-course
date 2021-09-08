e231f1260a25d884d504108fa197138eaaf9a13c
Josh Cook <joshuacook0023@gmail.com>
2021-09-08 07:00:41 -0400

    exercise: cont. Assignment 3

--- src/plotting.R
+++ src/plotting.R
@@ -1,5 +1,5 @@
 
-
+# Plot a distribution using base plotting methods.
 plot_dist <- function(x, y, xlab, ylab, ...) {
   plot(
     x,
@@ -11,4 +11,28 @@ plot_dist <- function(x, y, xlab, ylab, ...) {
     yaxs = "i",
     ...
   )
+}
+
+
+# Plot two distributions using ggplot.
+plot_two_distributions <- function(x1, name0, x2, name1) {
+  bind_rows(
+    tibble(x = x1, name = name0),
+    tibble(x = x2, name = name1)
+  ) %>%
+    ggplot(aes(x = x)) +
+    geom_density(aes(color = name, fill = name), alpha = 0.2) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+    theme(legend.position = c(0.85, 0.7))
+}
+
+
+# Plot a single distribution using ggplot.
+plot_single_distribution <- function(x) {
+  tibble(x = x) %>%
+    ggplot(aes(x = x)) +
+    geom_density() +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.02)))
 }
