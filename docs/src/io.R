08bfcec992b3f426e7150274aacc20c411b16ca1
Josh Cook <joshuacook0023@gmail.com>
2021-09-25 09:59:20 -0400

    chore: render site

--- docs/src/io.R
+++ docs/src/io.R
@@ -5,6 +5,13 @@ data_file_path <- function(file_name) {
 }
 
 # Read data from the "data/" directory.
-read_data <- function(file_name, .f = readLines) {
-  return(.f(data_file_path(file_name)))
+read_data <- function(file_name, .f = readLines, ...) {
+  return(.f(data_file_path(file_name), ...))
+}
+
+# This file has weird formatting so requires special processing.
+read_bioassay_data <- function(...) {
+  read_data("bioassay.txt", read_tsv, ...) %>%
+    separate(`x n y`, into = c("x", "n", "y"), sep = " +") %>%
+    mutate_all(as.numeric)
 }
