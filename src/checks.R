1fa38fdbdd63b50a91e83a40e62ffb2bd18645b2
Josh Cook <joshuacook0023@gmail.com>
2021-09-25 09:57:36 -0400

    exercise: finish Assignment 4

--- src/checks.R
+++ src/checks.R
@@ -1,5 +1,19 @@
 
 # Check is two values (or vectors) are close to each other.
 close_to <- function(x, y, epsilon = 0.01) {
-  return(abs(x - y) < epsilon)
+  check_lengths <- length(x) == length(y)
+  check_diff <- abs(x - y) < epsilon
+  return(check_lengths & check_diff)
+}
+
+# Stop if the observed value is not close to the expected value.
+# All inputs are passed to `close_to()`.
+stop_if_not_close_to <- function(...) {
+  stopifnot(close_to(...))
+}
+
+# Stop if not all observed values are close to expected values.
+# All inputs are passed to `close_to()`.
+stop_if_not_all_close_to <- function(...) {
+  stopifnot(all(close_to(...)))
 }
